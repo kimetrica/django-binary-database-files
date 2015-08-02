452c8271c49d72003482eae063e9a69bf99f927e
Richard Xia <richardxia@richardxia.com>
2015-08-01 22:53:08 -0700

    Add Django-1.7-style migrations.

---
+++ database_files/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='File',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=255, db_index=True)),
+                ('size', models.PositiveIntegerField(db_index=True)),
+                ('_content', models.TextField(db_column=b'content')),
+                ('created_datetime', models.DateTimeField(default=django.utils.timezone.now, verbose_name=b'Created datetime', db_index=True)),
+                ('_content_hash', models.CharField(db_index=True, max_length=128, null=True, db_column=b'content_hash', blank=True)),
+            ],
+            options={
+                'db_table': 'database_files_file',
+            },
+        ),
+    ]
