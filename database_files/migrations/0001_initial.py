81e22c317b335463d41fda66b43c586cb5cdbe3f
Roger Hunwicks <roger@tonic-solutions.com>
2015-08-23 15:41:03 +0600

    Fix error introduced when merging Django 1.7+ migrations

--- database_files/migrations/0001_initial.py
+++ database_files/migrations/0001_initial.py
@@ -4,8 +4,6 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import django.utils.timezone
 
-from south.db import db
-from south.v2 import SchemaMigration
 
 class Migration(migrations.Migration):
 
@@ -20,8 +18,8 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(unique=True, max_length=255, db_index=True)),
                 ('size', models.PositiveIntegerField(db_index=True)),
                 ('content', models.BinaryField()),
-                ('created_datetime', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Created datetime', db_index=True)),
-                ('_content_hash', models.CharField(db_index=True, max_length=128, null=True, db_column='content_hash', blank=True)),
+                ('created_datetime', models.DateTimeField(default=django.utils.timezone.now, verbose_name=b'Created datetime', db_index=True)),
+                ('_content_hash', models.CharField(db_index=True, max_length=128, null=True, db_column=b'content_hash', blank=True)),
             ],
             options={
                 'db_table': 'database_files_file',
