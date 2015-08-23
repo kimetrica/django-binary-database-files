6b7e8e8a50eb4bf9c6d50bb376678876242c493a
Roger Hunwicks <roger@tonic-solutions.com>
2015-08-23 15:41:03 +0600

    Fix error introduced when merging Django 1.7+ migrations

--- database_files/migrations/0001_initial.py
+++ database_files/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=255, db_index=True)),
                 ('size', models.PositiveIntegerField(db_index=True)),
-                ('content', self.gf('django.db.models.fields.BinaryField')()),
+                ('content', models.BinaryField()),
                 ('created_datetime', models.DateTimeField(default=django.utils.timezone.now, verbose_name=b'Created datetime', db_index=True)),
                 ('_content_hash', models.CharField(db_index=True, max_length=128, null=True, db_column=b'content_hash', blank=True)),
             ],
