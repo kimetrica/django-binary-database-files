71b42d288b68b2bac07f8e423429971e68a7ffd5
Roger Hunwicks <roger@tonic-solutions.com>
2016-02-04 10:55:44 +0600

    Make Django 1.7 migrations compatible with Python3

--- database_files/migrations/0001_initial.py
+++ database_files/migrations/0001_initial.py
@@ -17,9 +17,9 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=255, db_index=True)),
                 ('size', models.PositiveIntegerField(db_index=True)),
-                ('_content', models.TextField(db_column=b'content')),
-                ('created_datetime', models.DateTimeField(default=django.utils.timezone.now, verbose_name=b'Created datetime', db_index=True)),
-                ('_content_hash', models.CharField(db_index=True, max_length=128, null=True, db_column=b'content_hash', blank=True)),
+                ('_content', models.TextField(db_column='content')),
+                ('created_datetime', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Created datetime', db_index=True)),
+                ('_content_hash', models.CharField(db_index=True, max_length=128, null=True, db_column='content_hash', blank=True)),
             ],
             options={
                 'db_table': 'database_files_file',
