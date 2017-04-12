8feeaed64dcbef6c490f50358716f5d28ceffda4
Jesaja Everling <jeverling@gmail.com>
2017-04-12 16:35:27 +0300

    AppConfig for compatibility with 1.7+ app loading

--- database_files/__init__.py
+++ database_files/__init__.py
@@ -1,2 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, division
+
+
 VERSION = (0, 3, 4)
+
+
 __version__ = '.'.join(map(str, VERSION))
+
+
+default_app_config = 'database_files.apps.DatabaseFilesAppConfig'
