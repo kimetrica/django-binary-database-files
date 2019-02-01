6793e7ff206b63d11bb43f8d2050a3efbfb7afee
Johannes Wilm <mail@johanneswilm.org>
2019-02-01 12:14:31 +0100

    unified re_path import

--- database_files/urls.py
+++ database_files/urls.py
@@ -1,8 +1,12 @@
-from django.urls import url
+try:
+    from django.urls import re_path
+except ImportError:
+    # Django 1.11
+    from django.conf.urls import url as re_path
 from database_files import views
 
 urlpatterns = [
-    url(r'^files/(?P<name>.+)$',
+    re_path(r'^files/(?P<name>.+)$',
         views.serve_mixed,
         name='database_file'),
 ]
