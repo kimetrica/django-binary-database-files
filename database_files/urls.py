6648bf2bb11d751a18cdde75b802a902d5ea83ff
Johannes Wilm <mail@johanneswilm.org>
2019-02-01 12:08:14 +0100

    re_path => url (for django 1.11)

--- database_files/urls.py
+++ database_files/urls.py
@@ -1,14 +1,8 @@
-
-from django.urls import re_path
-
+from django.urls import url
 from database_files import views
 
-
 urlpatterns = [
-#    url(r'^files/(?P<name>.+)$',
-#        'database_files.views.serve',
-#        name='database_file'),
-    re_path(r'^files/(?P<name>.+)$',
+    url(r'^files/(?P<name>.+)$',
         views.serve_mixed,
         name='database_file'),
 ]
