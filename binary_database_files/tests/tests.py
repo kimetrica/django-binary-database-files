0e8a3c1c013dfedaa18ce32588ac00bf6b8ad40c
Johannes Wilm <mail@johanneswilm.org>
2019-02-01 13:24:11 +0100

    getvalue

--- binary_database_files/tests/tests.py
+++ binary_database_files/tests/tests.py
@@ -56,7 +56,7 @@ class DatabaseFilesTestCase(TestCase):
         File.objects.update()
         q = File.objects.all()
         self.assertEqual(q.count(), 1)
-        self.assertEqual(q.first().content.toString(), b"hello there")
+        self.assertEqual(q.first().content.getvalue(), b"hello there")
 
         # Load a dynamically created file outside /media.
         test_file = files.temp.NamedTemporaryFile(
