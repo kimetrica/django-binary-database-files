a24944e16bba151107ca96934ad2f1b68a04e89e
Johannes Wilm <mail@johanneswilm.org>
2020-01-29 19:00:33 +0100

    1.0.10: add relevant tags

--- binary_database_files/__init__.py
+++ binary_database_files/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, division
 
-VERSION = (1, 0, 9)
+VERSION = (1, 0, 10)
 __version__ = '.'.join(map(str, VERSION))
 
 default_app_config = 'binary_database_files.apps.DatabaseFilesAppConfig'
