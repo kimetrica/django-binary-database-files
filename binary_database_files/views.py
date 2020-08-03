2df0f299d9443e26d94cfc4f1e111a1b23a22b58
Joerg Sonnenberger <joerg@bec.de>
2020-08-03 20:16:12 +0200

    Honor DB_FILES_AUTO_EXPORT_DB_TO_FS in the serve view

--- binary_database_files/views.py
+++ binary_database_files/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.cache import cache_control
 from django.views.static import serve as django_serve
 
 from binary_database_files.models import File
-
+from binary_database_files import settings as _settings
 
 @cache_control(max_age=86400)
 def serve(request, name):
@@ -15,7 +15,8 @@ def serve(request, name):
     Retrieves the file from the database.
     """
     f = get_object_or_404(File, name=name)
-    f.dump()
+    if _settings.DB_FILES_AUTO_EXPORT_DB_TO_FS:
+        f.dump()
     mimetype = mimetypes.guess_type(name)[0] or "application/octet-stream"
     # Cast to bytes to work around https://code.djangoproject.com/ticket/30294
     response = HttpResponse(bytes(f.content), content_type=mimetype)
